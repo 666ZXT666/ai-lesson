882321e2fe15a99c95b03f69ea40daed764eb78f
zxt <3210835162@qq.com>
2024-11-10 10:53:46 +0800

    11.10:

---
+++ lessons/ai/slb/main.mjs
@@ -0,0 +1,16 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    //付费
+  apiKey:'sk-5PUDrQhFqCVr0KluhSbYcCgIx6S21dNcxGJOxhpo3BEtIhRw',
+ // 国内转发
+  baseURL: 'Https://api.302.ai/v1',
+});
+
+//向openai发送请求
+//完成生成 GC
+const response = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: '假如你是林夕这样的爱情歌曲大家，请你以写一首主题为爱上森林北的摇滚曲，森林北是一位美丽，洒脱，有活力的女孩。',
+})
+console.log(response);
