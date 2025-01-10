f7511f07781a2bb3d614da506d6c4112866f4e33
ZXT <3210835162@qq.com>
2025-01-10 11:35:46 +0800

    jsonp

--- lessons/js/jsonp/server.js
+++ lessons/js/jsonp/server.js
@@ -9,12 +9,13 @@ const users = [
 ];
 
 const server = http.createServer((req, res) => {
+  //res.end('let a ='+JSON.stringify(users));
   res.end("callback" + "(" + JSON.stringify(users) + ")");
-  //异步 高并发
-  //请求带到服务器后，函数会执行
+  // 异步 高并发
+  // 请求带到服务器后，函数会执行
 });
 
-server.listen(3000, () => {
+server.listen(8080, () => {
 console.log('server is running at http://localhost:3000');
 });
 
