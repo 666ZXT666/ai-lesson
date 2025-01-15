0aecef48dd710e2e703e9c5c3c8d0d78a58df7fd
ZXT <3210835162@qq.com>
2025-01-15 20:09:25 +0800

    1

--- lessons/vueAndReact/vue-router-demo/src/views/show/Post-show.vue
+++ lessons/vueAndReact/vue-router-demo/src/views/show/Post-show.vue
@@ -1,22 +1,20 @@
 <template>
+  <div>
     <div>
-        <div>Post-show{{ postId }}</div>
+      <div>Post-show{{ postId }}</div>
     </div>
     <router-view></router-view>
-    </template>
+  </div>
+</template>
     
     <script setup>
-    import{defineProps} from 'vue' // 定义props
-    import { useRoute } from 'vue-router' // 获取路由参数
-    const route = useRoute()
-    const {postId} = route.params.postId
-
-
-
-    </script>
+import { defineProps } from "vue"; // 定义props
+import { useRoute } from "vue-router"; // 获取路由参数
+const route = useRoute();
+const { postId } = route.params.postId;
+</script>
     
     <style scoped>
-    
-    </style>
+</style>
     
     
